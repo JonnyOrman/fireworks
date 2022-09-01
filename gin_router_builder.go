9b8a93a7cc6e706a45425d41bc4570b55eda76d3
Jonathan Orman <jonathanpcorman@gmail.com>
2022-09-01 13:38:21 +0100

    Add GinRouterBuilder

---
+++ gin_router_builder.go
@@ -0,0 +1,23 @@
+package fireworks
+
+import "github.com/gin-gonic/gin"
+
+type GinRouterBuilder struct {
+	router *gin.Engine
+}
+
+func NewGinRouterBuilder() *GinRouterBuilder {
+	this := new(GinRouterBuilder)
+
+	this.router = gin.Default()
+
+	return this
+}
+
+func (this GinRouterBuilder) AddPost(routePath string, handler gin.HandlerFunc) {
+	this.router.POST(routePath, handler)
+}
+
+func (this GinRouterBuilder) Build() *gin.Engine {
+	return this.router
+}
